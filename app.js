7d97d67840f16c68a9fb1fcfc8c59f59c1321c08
durgesh7292 <durgeshkumarchy@gmail.com>
2024-05-28 16:35:58 +0530

    added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config({ path: "./config/config.env" });
 app.use(
   cors({
-    origin: ["https://restaurant3-hkzc.onrender.com",],
+    origin: process.env.frontend_url,
 
     method: ["POST"],
     credentials: true,
