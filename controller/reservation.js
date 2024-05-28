5e24be2b8dbdb8b88e00c46b24a01e6b14b77bdf
durgesh7292 <durgeshkumarchy@gmail.com>
2024-05-28 16:22:20 +0530

    added

--- controller/reservation.js
+++ controller/reservation.js
@@ -1,35 +1,36 @@
 import { json } from "express";
 import ErrorHandler from "../error/error.js";
 import { Reservation } from "../models/reservationSchema.js";
-
 export const sendReservation = async (req, res, next) => {
+  i;
   const { firstName, LastName, email, phone, time, date } = req.body;
-  //console.log("email: ", email);
-
-  if ([firstName, LastName, email, phone, time, date].some((field) => field?.trim() === "")) {
-    return next(new ErrorHandler(400, "All fields are required"));
+  if (!firstName || !LastName || !email || !phone || !date || !time) {
+    return next(new ErrorHandler("please full fill reservation from!", 400));
+  }
+  const existingReservation = await Reservation.findOne({ email });
+  if (existingReservation) {
+    return next(new ErrorHandler("A reservation already exists for the selected emai.", 400));
   }
 
-  const existedUser = await Reservation.findOne({ email });
+  try {
+    await Reservation.create({
+      firstName,
+      LastName,
+      email,
+      phone,
+      time,
+      date,
+    });
 
-  if (existedUser) {
-    return next(new ErrorHandler(400, "User with email or username already exists"));
+    res.status(200).json({
+      success: "true",
+      message: "Reservation sent Sucessfully!",
+    });
+  } catch (error) {
+    if (error.name === "validationError") {
+      const validationErrors = object.value(error.errors).map((err) => err.message);
+      return next(new ErrorHandler(validationErrors.join(","), 400));
+    }
+    return next(error);
   }
-
-  const user = await Reservation.create({
-    firstName,
-    LastName,
-    email,
-    phone,
-    time,
-    date,
-  });
-
-  const createdUser = await Reservation.findById(user.email);
-
-  if (!createdUser) {
-    return next(new ErrorHandler(400, "Something went wrong while registering the user"));
-  }
-
-  return res.status(201).json(new ErrorHandler(200, createdUser, "User registered Successfully"));
 };
