6ff33d99c5fa611b78e454d3112b7d24e23dc41b
durgesh7292 <durgeshkumarchy@gmail.com>
2024-05-28 15:49:34 +0530

    modified

--- controller/reservation.js
+++ controller/reservation.js
@@ -2,10 +2,16 @@ import { json } from "express";
 import ErrorHandler from "../error/error.js";
 import { Reservation } from "../models/reservationSchema.js";
 export const sendReservation = async (req, res, next) => {
+  i;
   const { firstName, LastName, email, phone, time, date } = req.body;
   if (!firstName || !LastName || !email || !phone || !date || !time) {
     return next(new ErrorHandler("please full fill reservation from!", 400));
   }
+  const existingReservation = await Reservation.findOne({ email });
+  if (existingReservation) {
+    return next(new ErrorHandler("A reservation already exists for the selected emai.", 400));
+  }
+
   try {
     await Reservation.create({
       firstName,
@@ -15,6 +21,7 @@ export const sendReservation = async (req, res, next) => {
       time,
       date,
     });
+
     res.status(200).json({
       success: "true",
       message: "Reservation sent Sucessfully!",
--- models/reservationSchema.js
+++ models/reservationSchema.js
@@ -21,8 +21,8 @@ const reservationSchema = new mongoose.Schema({
   phone: {
     type: String,
     required: true,
-    minLength: [11, "Phone number must contain only 11 digits!"],
-    maxLength: [11, " Phone number must contain only 11 digits!"],
+    minLength: [10, "Phone number must contain only 10 digits!"],
+    maxLength: [10, " Phone number must contain only 10 digits!"],
   },
   time: {
     type: String,
